83251bc88f1cf1941f5f5c3b4db0c2f6ce47b6f4
Prashant <prashant.shahpure@gmail.com>
2021-06-30 17:57:30 +0530

    added searchPageTest in Testcases

---
+++ src/test/java/com/crm/qa/testcases/SearchPageTest.java
@@ -0,0 +1,14 @@
+package com.crm.qa.testcases;
+
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+
+public class SearchPageTest extends TestBase{
+
+	
+	@Test
+	public void searchPageTest() {
+		System.out.println("This is search page to check push in GIT");
+	}
+}
